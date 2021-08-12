c5ebcdb94e69496f3f2fee938bac1ea9d04fc3bb
arbabali1441 <arbabali1441@gmail.com>
2021-08-12 17:37:27 +0500

    completed till @ add icon button 32:11

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
 
 function Header() {
   return (
     <div>
       <h2>I am header</h2>
-      <PersonIcon />
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
     </div>
   )
 }
